7405d9e37bef895d3ded2bc2c8d593fd2b8b6422
Guilherme Martinez Sampaio <guilhermesampaio@gmail.com>
2013-07-25 19:22:49 -0300

    Header file to import all library source code

---
+++ GSTest/GSTest/GSTest/GSTest.h
@@ -0,0 +1,35 @@
+//
+//  GSTest.h
+//  GSTest
+//
+//  Copyright (c) 2013 Guilherme Sampaio. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+
+#import <Foundation/Foundation.h>
+
+#ifndef GSTest_GSTest_h
+#define GSTest_GSTest_h
+    #import "GSNetworkMockURLProtocol.h"
+    #import "GSNetworkMockData.h"
+    #import "GSNetworkMockDataParameter.h"
+    #import "GSNetworkMockDataJSONParameter.h"
+    #import "GSNetworkMockDataStringParameter.h"
+#endif
